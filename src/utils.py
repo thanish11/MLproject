2f052d83150e852177e08cabb3dd4d34818dec5e
thanish11 <thanishsaravana2001@gmail.com>
2024-07-06 11:50:45 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,10 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -19,22 +21,31 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, X_test, y_train, y_test, models):
+def evaluate_models(X_train, X_test, y_train, y_test, models, param):
     try:
-        report={}
+        report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            logging.info(f"Evaluating model: {model_name}")
+            para = param.get(model_name, {})
 
-            model.fit(X_train, y_train)
+            if para:  # Check if there are parameters to tune
+                logging.info(f"Tuning parameters for {model_name}")
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                best_model = gs.best_estimator_
+            else:
+                logging.info(f"No parameters to tune for {model_name}")
+                best_model = model
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-            
+            best_model.fit(X_train, y_train)
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
